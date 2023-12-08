5cf2f1cb76929add8eabe214ff0f65db6c8bf2a2
guilherme <guilherme.marini@aluno.senai.br>
2023-12-08 14:40:23 -0300

    criei o edit joia e relogio no admim

--- admin/editJoia.php
+++ admin/editJoia.php
@@ -1,8 +1,8 @@
 <?php
-$titulo_pagina = "editar cliente";
+$titulo_pagina = "editar joia";
 require_once "./template/header.php";
 require_once "./template/navbar.php";
 require_once "./template/sidebar.php";
-require_once "./template/main-edit-cliente.php";
+require_once "./template/main-edit-joia.php";
 require_once "./template/footer.php";
 ?>
