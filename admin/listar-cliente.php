faec63afe176d40bfe453fa79e0daf7aa2899324
guilherme <guilherme.marini@aluno.senai.br>
2023-12-08 12:22:36 -0300

    criei o edit relogio0

--- admin/listar-cliente.php
+++
@@ -1,8 +0,0 @@
-<?php
-$titulo_pagina = "Listar";
-require_once "./template/header.php";
-require_once "./template/navbar.php";
-require_once "./template/sidebar.php";
-require_once "./template/main-cad-listar-cliente.php";
-require_once "./template/footer.php";
-?>
